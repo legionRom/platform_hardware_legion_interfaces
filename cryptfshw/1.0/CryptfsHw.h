6c3f451b19729bd739b3637365b882055b7f6a3b
rajkale99 <kaleraj.rk@gmail.com>
2020-05-23 10:30:12 +0000

    travk los17.1 changes

--- cryptfshw/1.0/CryptfsHw.h
+++ cryptfshw/1.0/CryptfsHw.h
@@ -16,59 +16,42 @@
 
 #pragma once
 
-#include <hidl/MQDescriptor.h>
-#include <hidl/Status.h>
+#include <android-base/macros.h>
 #include <vendor/qti/hardware/cryptfshw/1.0/ICryptfsHw.h>
+
+#include <memory>
+
+#include "ICryptfsHwController.h"
 
 namespace vendor {
 namespace qti {
 namespace hardware {
 namespace cryptfshw {
 namespace V1_0 {
-namespace ioctl_qti {
+namespace implementation {
 
-using ::android::sp;
-using ::android::hardware::hidl_array;
-using ::android::hardware::hidl_memory;
 using ::android::hardware::hidl_string;
-using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
-using ::android::hardware::Void;
-
-#define QTI_ICE_STORAGE_NOT -1
-#define QTI_ICE_STORAGE_UFS 1
-#define QTI_ICE_STORAGE_SDCC 2
-
-#define CRYPTFS_HW_KMS_WIPE_KEY 1
-#define CRYPTFS_HW_CREATE_KEY_FAILED -7
-#define CRYPTFS_HW_WIPE_KEY_FAILED -8
-#define CRYPTFS_HW_UPDATE_KEY_FAILED -9
-#define CRYPTFS_HW_KMS_MAX_FAILURE -10
-
-enum cryptfs_hw_key_management_usage_type {
-    CRYPTFS_HW_KM_USAGE_DISK_ENCRYPTION = 0x01,
-    CRYPTFS_HW_KM_USAGE_FILE_ENCRYPTION = 0x02,
-    CRYPTFS_HW_KM_USAGE_UFS_ICE_DISK_ENCRYPTION = 0x03,
-    CRYPTFS_HW_KM_USAGE_SDCC_ICE_DISK_ENCRYPTION = 0x04,
-    CRYPTFS_HW_KM_USAGE_MAX
-};
 
 class CryptfsHw : public ICryptfsHw {
-   public:
-    CryptfsHw();
+  public:
+    CryptfsHw(std::unique_ptr<ICryptfsHwController> controller);
 
     // Methods from ::vendor::qti::hardware::cryptfshw::V1_0::ICryptfsHw follow.
     Return<int32_t> setIceParam(uint32_t flag) override;
-    Return<int32_t> setKey(const hidl_string& passwd, const hidl_string&) override;
+    Return<int32_t> setKey(const hidl_string& passwd, const hidl_string& enc_mode) override;
     Return<int32_t> updateKey(const hidl_string& oldpw, const hidl_string& newpw,
-                              const hidl_string&) override;
+                              const hidl_string& enc_mode) override;
     Return<int32_t> clearKey() override;
 
-   private:
-    int mStorageType;
+  private:
+    std::unique_ptr<ICryptfsHwController> controller_;
+    int usage_;
+
+    DISALLOW_IMPLICIT_CONSTRUCTORS(CryptfsHw);
 };
 
-}  // namespace ioctl_qti
+}  // namespace implementation
 }  // namespace V1_0
 }  // namespace cryptfshw
 }  // namespace hardware
